55e71cf5744a72e240744002855a58581bf9437b
Louis2324 <miguellouis2023@gmail.com>
2025-11-24 10:35:26 +0200

    Added theme persistence on tab reload scoped to the device's browser

--- content/content.js
+++ content/content.js
@@ -1,22 +1,61 @@
-console.log(
-  "Theme Switcher content script LOADED on:",
-  window.location.href
-);
+console.log("Theme Switcher content script LOADED on:", window.location.href);
+
+applySavedTheme();
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log("Message received:", request);
 
   if (request.action === "applyDarkTheme") {
-    console.log("Applying dark theme...");
-    document.body.style.backgroundColor = "#333";
-    document.body.style.color = "white";
+    applyDarkTheme();
+    saveTheme("dark");
   } else if (request.action === "applyLightTheme") {
-    console.log("Applying light theme...");
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
+    applyLightTheme();
+    saveTheme("light");
   } else if (request.action === "resetTheme") {
-    console.log("Resetting theme...");
-    document.body.style.backgroundColor = "";
-    document.body.style.color = "";
+    resetTheme();
+    saveTheme("reset");
   }
 });
+
+/*theme functions*/
+
+function applyDarkTheme() {
+  document.body.style.backgroundColor = "#333";
+  document.body.style.color = "white";
+}
+
+function applyLightTheme() {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+}
+
+function resetTheme() {
+  document.body.style.backgroundColor = "";
+  document.body.style.color = "";
+}
+
+function saveTheme(themeName) {
+  chrome.storage.local.set(
+    {
+      currentTheme: themeName,
+    },
+    function () {
+      console.log("Theme saved:", themeName);
+    }
+  );
+}
+
+/*apply saved storage theme*/
+
+function applySavedTheme() {
+  chrome.storage.local.get(["currentTheme"], function (result) {
+    const savedTheme = result.currentTheme;
+    console.log("Loaded saved theme:", savedTheme);
+
+    if (savedTheme === "dark") {
+      applyDarkTheme();
+    } else if (savedTheme === "light") {
+      applyLightTheme();
+    }
+  });
+}
